01216b8cd1f0a16aa3b5fe3b1d44e03b668f0ccd
alexg622 <alex.gm62288@gmail.com>
2018-08-30 02:03:44 -0700

    A lot

---
+++ albums/src/components/header.js
@@ -0,0 +1,34 @@
+// import libraries for component
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+// make component
+const Header = (props) => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>{props.headerText}</Text>
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: "#F8F8F8",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    paddingTop: 15,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+})
+
+// make component available to other parts of app
+export default Header
