a3dbfbf16fb9bd076d6f9d647b50fa8c7f7b20e3
alexg622 <alex.gm62288@gmail.com>
2018-08-31 16:53:41 -0700

    Make better

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, TouchableWithoutFeedback, View } from 'react-native'
 import { CardSection } from './common'
+import * as actions from '../actions'
+import { connect } from 'react-redux'
 
 class ListItem extends Component {
   constructor(props){
     super(props)
   }
 
+  renderDescription(){
+    const {library, selectedLibraryId} = this.props
+    if(library.item.id === this.props.selectedLibraryId){
+      return (
+        <Text>{library.item.description}</Text>
+      )
+    }
+  }
+
   render(){
+    const { id, title } = this.props.library.item
+    const { titleStyle } = styles
+
     return (
-      <CardSection>
-        <Text>{this.props.library.item.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+        >
+        <View>
+          <CardSection>
+            <Text style={ titleStyle }>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
 
-export default ListItem
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    selectedLibraryId: state.selectedLibraryId
+  }
+}
+
+export default connect(mapStateToProps, actions)(ListItem)
